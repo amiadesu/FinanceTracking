63e6caaeb90fb61fb00fd6181e5d95efc164b4c7
Amia <67781417+amiadesu@users.noreply.github.com>
2026-02-21 10:21:05 +0200

    Added @nuxt/icon.

--- src/frontend/nuxt.config.ts
+++ src/frontend/nuxt.config.ts
@@ -45,6 +45,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
+    '@nuxt/icon',
     '@nuxtjs/color-mode',
     'nuxt-oidc-auth'
   ],
