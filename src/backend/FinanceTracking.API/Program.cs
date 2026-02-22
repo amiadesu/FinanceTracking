1f7812cc51f09764bb619294ab686afecffc8283
Amia <67781417+amiadesu@users.noreply.github.com>
2026-02-22 11:49:20 +0200

    Added DB migration pipeline inside FinanceTracking.API.

--- src/backend/FinanceTracking.API/Program.cs
+++ src/backend/FinanceTracking.API/Program.cs
@@ -1,10 +1,35 @@
+using FinanceTracking.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// 1. Add Services
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<FinanceDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+           .UseSnakeCaseNamingConvention());
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<FinanceDbContext>();
+        await context.Database.MigrateAsync();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
