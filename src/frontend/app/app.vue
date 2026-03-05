53c5b95b65fa2e88b4e69cd338690c6b5e456755
Amia <67781417+amiadesu@users.noreply.github.com>
2026-03-05 16:51:40 +0200

    Updated `app.vue` page following Getting started guide for `@nuxt/ui`.

--- src/frontend/app/app.vue
+++ src/frontend/app/app.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="min-h-screen flex flex-col bg-white text-gray-900">
-    <NavBar class="h-16" />
+  <UApp>
+    <div class="min-h-screen flex flex-col bg-white text-gray-900">
+      <NavBar class="h-16" />
 
-    <main class="flex-grow flex flex-col items-center justify-center px-4 py-16 text-center">
-      <NuxtLayout>
-        <NuxtPage />
-      </NuxtLayout>
-    </main>
-  </div>
+      <main class="flex-grow flex flex-col items-center justify-center px-4 py-16 text-center">
+        <NuxtLayout>
+          <NuxtPage />
+        </NuxtLayout>
+      </main>
+    </div>
+  </UApp>
 </template>
 
 <script setup lang="ts">
