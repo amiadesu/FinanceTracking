8690ffd083fb2ef2712b0b1abc899cf8d07fc79c
Amia <67781417+amiadesu@users.noreply.github.com>
2026-03-07 09:40:50 +0200

    Made smaller vertical padding.

--- src/frontend/app/app.vue
+++ src/frontend/app/app.vue
@@ -3,7 +3,7 @@
     <div class="min-h-screen flex flex-col bg-white text-gray-900">
       <NavBar class="h-16" />
 
-      <main class="flex-grow flex flex-col items-center justify-center px-4 py-16 text-center">
+      <main class="flex-grow flex flex-col items-center justify-center px-4 py-4 text-center">
         <NuxtLayout>
           <NuxtPage />
         </NuxtLayout>
