a5a544ce77021b7644b9ae6ea5c06a23e58ad028
Amia <67781417+amiadesu@users.noreply.github.com>
2026-03-07 16:16:46 +0200

    Added composable and component for global validation error display inside forms.

---
+++ src/frontend/app/components/FormGlobalErrors.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  errors: string[]
+}>();
+</script>
+
+<template>
+  <div v-if="errors && errors.length > 0">
+    <p 
+      v-for="(error, index) in errors" 
+      :key="index" 
+      class="text-sm text-red-500 dark:text-red-400 mt-2 font-normal"
+    >
+      {{ error }}
+    </p>
+  </div>
+</template>
