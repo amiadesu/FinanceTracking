3f9d87ffa5050e452898a03050e43eccc29fa518
Amia <67781417+amiadesu@users.noreply.github.com>
2026-03-07 09:18:24 +0200

    Created separate `schemas.ts` file for storing validation schemas.

---
+++ src/frontend/app/schemas/schemas.ts
@@ -0,0 +1,41 @@
+import * as v from 'valibot';
+
+export const inviteSchema = v.object({
+    targetUserIdentifier: v.pipe(v.string(), v.minLength(1, 'Email or Username is required')),
+    note: v.optional(v.pipe(v.string(), v.maxLength(500, 'Note must be less than 500 characters')))
+});
+
+export const categorySchema = v.object({
+    name: v.pipe(
+        v.string(),
+        v.nonEmpty('Name is required'),
+        v.maxLength(100, 'Name must be less than 100 characters')
+    ),
+    colorHex: v.pipe(
+        v.string(),
+        v.regex(/^#([0-9A-Fa-f]{6})$/, 'Color must be a valid hex code')
+    )
+});
+
+export const budgetGoalSchema = v.pipe(
+    v.object({
+        targetAmount: v.pipe(
+            v.number(),
+            v.minValue(1, 'Target amount must be greater than 0')
+        ),
+        startDate: v.pipe(
+            v.string(),
+            v.nonEmpty('Start date is required'),
+            v.isoDate('Start date must be a valid date')
+        ),
+        endDate: v.pipe(
+            v.string(),
+            v.nonEmpty('End date is required'),
+            v.isoDate('End date must be a valid date')
+        )
+    }),
+    v.check(
+        (input) => input.startDate < input.endDate, 
+        'Start date must be before end date'
+    )
+);
