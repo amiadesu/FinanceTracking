9655ed05d591f2287e8854e306910f4c7b51bd0a
Amia <67781417+amiadesu@users.noreply.github.com>
2026-03-08 10:55:06 +0200

    Slightly optimized RAM usage during build phase of frontend docker container.

--- src/frontend/nuxt.config.ts
+++ src/frontend/nuxt.config.ts
@@ -59,6 +59,10 @@ export default defineNuxtConfig({
   pages: true,
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  sourcemap: {
+    server: false,
+    client: false
+  },
   modules: [
     '@nuxt/ui',
     '@nuxt/icon',
